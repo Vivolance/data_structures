19ea163ae53b6c95887885ca1fd9e234c2de32d7
Elson Chan <elsonccjgmail.com>
2025-03-03 17:30:12 +0800

    Added dfs practice

---
+++ src/7_DFS/dfs_practice_1.py
@@ -0,0 +1,20 @@
+"""
+Implement a DEPTH FIRST SEARCH (DFS) suing a stack. For simplicity, we always use a deque to implement this.
+DFS uses a stack in its implementation.
+
+Example graph represented as an adjacency list.
+    graph: Dict[int, List[int]] = {
+        0: [1, 2],
+        1: [0, 3, 4],
+        2: [0, 5],
+        3: [1],
+        4: [1, 5],
+        5: [2, 4]
+    }
+
+"""
+
+
+
+def dfs(graph: dict[int, list[int]]) -> list[int]:
+    pass
