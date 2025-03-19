7e0aa91629b0fbcbe9a3e27efe6d92b69877eef9
Elson Chan <elsonccjgmail.com>
2025-03-20 02:27:10 +0800

    Added pyspark types

--- src/6_spark/4b_pandas_udf.py
+++ src/6_spark/4b_pandas_udf.py
@@ -4,25 +4,31 @@ Pandas UDFs
 - Does not need to be registered to the SparkSession
 - Use Pandas capabilities for large datasets
 """
+
 from typing import Any
 
 from pyspark.shell import spark
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import pandas_udf
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+    FloatType,
+)
 
 data: list[dict[str, Any]] = [
     {"id": 1, "name": "Elson", "temp_in_f": 200.09},
     {"id": 2, "name": "Clement", "temp_in_f": 180.20},
     {"id": 3, "name": "Eugene", "temp_in_f": 100.77},
-
 ]
 
 scheme = StructType(
     [
         StructField("id", IntegerType(), False),
         StructField("name", StringType(), True),
-        StructField("temp_in_f", FloatType(), True)
+        StructField("temp_in_f", FloatType(), True),
     ]
 )
 
@@ -31,9 +37,11 @@ user_temp_df: DataFrame = spark.createDataFrame(data, scheme)
 
 @pandas_udf(FloatType())
 def f_to_c(column):
-    return (column - 32) * 5.0/9.0
+    return (column - 32) * 5.0 / 9.0
 
 
 # Call the pandas_udf function on the temp_in_f column
-user_temp_df_in_c = user_temp_df.withColumn("temp_in_c", f_to_c(user_temp_df["temp_in_f"]))
+user_temp_df_in_c = user_temp_df.withColumn(
+    "temp_in_c", f_to_c(user_temp_df["temp_in_f"])
+)
 user_temp_df_in_c.show()
