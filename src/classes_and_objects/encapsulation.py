4d6412017308cff3b226efe176f1ad997a9c9048
Elson Chan <elsonccjgmail.com>
2025-04-08 17:18:58 +0800

    Added encapsulation

---
+++ src/classes_and_objects/encapsulation.py
@@ -0,0 +1,49 @@
+"""
+Key Points:
+1. Use private or protected attributes to prevent accidental access of
+attributes outside the class
+2. Python uses name mangling for __age -> _ClassName_age, so that we cant
+do obj.__age and change its state easily.
+"""
+
+
+class Anything:
+    """
+    NEGATIVE EXAMPLE
+    """
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+
+class Anything2:
+    """
+    POSITIVE EXAMPLE
+    """
+    def __init__(self, name: str, age: int) -> None:
+        self._name = name
+        self.__age = age
+
+    def get_age(self) -> int:
+        return self.__age
+
+    def set_age(self, value: int) -> None:
+        self.__age = value
+
+
+if __name__ == "__main__":
+
+    # NEGATIVE EXAMPLE
+    obj = Anything("Clement", 25)
+    print(obj.age)  # prints 2
+    # Changes the attribute easily
+    obj.age = 100
+    print(obj.age) # prints 100
+
+    # POSITIVE EXAMPLE
+    obj2 = Anything2("Clement", 25)
+    # Safely accessing the age
+    print(obj2.get_age()) # returns 25
+    # Safely changes the age
+    obj2.set_age(30)
+    print(obj2.get_age())   # returns 30
