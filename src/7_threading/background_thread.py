7705071fbe4b55b067e0b46515799cf1148442ae
Elson Chan <elsonccjgmail.com>
2025-05-02 20:05:31 +0800

    Added threading practice

---
+++ src/7_threading/background_thread.py
@@ -0,0 +1,17 @@
+import threading
+
+
+def background_callable(name: str, age: int) -> None:
+    # simulate taking some time within the background callable
+    for i in range(999999999):
+        continue
+    print(f"{name} is {age} years old")
+
+
+if __name__ == "__main__":
+    print("main thread begins")
+    # Create a background thread that runs in the background
+    background_thread: threading.Thread = threading.Thread(target=background_callable, args=("Jason", 10))
+    background_thread.start()
+    background_thread.join()
+    print("end of background thread")
