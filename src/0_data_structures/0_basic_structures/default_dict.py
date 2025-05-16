eb283dc710a15111201d92dc58bc63469d44ec98
Elson Chan <elsonccjgmail.com>
2025-05-16 17:03:41 +0800

    added defaultdict example

---
+++ src/0_data_structures/0_basic_structures/default_dict.py
@@ -0,0 +1,20 @@
+"""
+A default dictionary is a dictionary that auto creates itself when a key that does not exist is passed in.
+"""
+from collections import defaultdict
+from typing import Any
+
+# initialise default dict, note that key is Any unless specified
+my_dict: defaultdict[Any, int] = defaultdict(int)
+
+# 1. Creating a key apple if not exist
+my_dict["apple"] += 1
+print(my_dict)
+# output: {'apple': 1}
+
+# 2. Word counting
+words: list[str] = ["orange", "pear", "pineapple", "guava", "orange", "pear", "pear"]
+for word in words:
+    my_dict[word] += 1
+print(my_dict)
+# output : {'apple': 1, 'orange': 2, 'pear': 3, 'pineapple': 1, 'guava': 1})
