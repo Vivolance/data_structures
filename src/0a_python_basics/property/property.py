d6fd1763c2677d2bbed51d6e7c9a828e829a0235
Elson Chan <elsonccj@gmail.com>
2025-06-21 14:59:33 +0800

    black ruff

--- src/0a_python_basics/property/property.py
+++ src/0a_python_basics/property/property.py
@@ -11,7 +11,7 @@ that changing one affects the other, consider using property
 class Square:
     def __init__(self, length: int) -> None:
         self.length = length
-        self.area = length ** 2
+        self.area = length**2
 
 
 # Good example, when you update length, area is changed automatically when you call it:
@@ -21,16 +21,16 @@ class Square2:
 
     @property
     def area(self) -> int:
-        return self.length ** 2
+        return self.length**2
 
 
 if __name__ == "__main__":
     square_1: Square = Square(5)
-    print(square_1.area)   # 25
-    square_1.length = 10   # update length
-    print(square_1.area)   # still 25
+    print(square_1.area)  # 25
+    square_1.length = 10  # update length
+    print(square_1.area)  # still 25
 
     square_2: Square2 = Square2(5)
-    print(square_2.area)    # 25
-    square_2.length = 10    # update length
-    print(square_2.area)    # changed to 100
+    print(square_2.area)  # 25
+    square_2.length = 10  # update length
+    print(square_2.area)  # changed to 100
